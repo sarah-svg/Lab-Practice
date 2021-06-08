07c685500905374ed256f913f69a9a0b074f709e
sarah-svg <kjskillen@gmail.com>
2021-06-07 23:13:07 -0700

    test3

--- _test_/getName.test.js
+++ _test_/getName.test.js
@@ -1,3 +1,4 @@
+import { copyAndPush } from '../copyAndPush';
 import { getName } from '../getName';
 
 describe('getName functon',  () => {
@@ -9,4 +10,8 @@ describe('getName functon',  () => {
     const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
     getName(character); 
   });
+  it('test the copy and push funtion', () => {
+    const numbers = [1, 2, 3, 4];
+    const actual = copyAndPush(numbers, 5);
+    expect(actual).toEqual(5);  });
 });
---
+++ copyAndPush.js
@@ -0,0 +1,5 @@
+export const copyAndPush = (numbers) => {
+ const newNumber = numbers.push(5);
+ console.log(newNumber);
+ return newNumber;
+};
