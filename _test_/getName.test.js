226add84786d6a14d8ba0e870cc43649281694e6
sarah-svg <kjskillen@gmail.com>
2021-06-07 22:57:42 -0700

    test 2 inside the test file

--- _test_/getName.test.js
+++ _test_/getName.test.js
@@ -5,5 +5,8 @@ describe('getName functon',  () => {
     const spot = { name: 'spot', age: 5, weight: '20 lbs' };
     const actual = getName(spot);
     expect(actual).toEqual('spot');
+
+    const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+    getName(character); 
   });
 });
